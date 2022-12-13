1f5d0513c9512e0c934713ee16fce70826bbe6b9
KlimKhur <mr.khurdakov@mail.ru>
2022-12-13 08:51:19 +0300

    Update script4.js

--- script4.js
+++ script4.js
@@ -1,11 +1,11 @@
 window.addEventListener("DOMContentLoaded", function () {
 
-               // Находим по id все input тега form
+
     let nameInput = document.getElementById("nameInput");
     let emailInput = document.getElementById("emailInput");
     let messageInput = document.getElementById("messageInput");
 
-        // Добавляем значения input в localStorage по вводу
+
     nameInput.addEventListener("input", function () {
         localStorage.setItem("nameInput", nameInput.value);
     });
@@ -16,28 +16,24 @@ window.addEventListener("DOMContentLoaded", function () {
         localStorage.setItem("messageInput", messageInput.value);
     });
 
-        // Задаем для всех input значения из localStorage
     nameInput.value = localStorage.getItem("nameInput");
     emailInput.value = localStorage.getItem("emailInput");
     messageInput.value = localStorage.getItem("messageInput");
 
-       // Находим по id кнопки открытия и закрытия формы и сам тег form
     let openFormButton = document.getElementById("openFormButton");
     let closeFormButton = document.getElementById("closeFormButton");
     let contactForm = document.getElementById("contactForm");
-
-    // По клику на кнопку открытия создаем новое состояние истории #contact-form, по клику на кнопку закрытия движемся назад по истории и убираем флажок с чекбокса
+  
+  
     openFormButton.addEventListener("click", function () {
         history.pushState("activeForm", null, "#contact-form");
-        // history.forward();
     });
     closeFormButton.addEventListener("click", function () {
         history.back();
         document.getElementById("checkbox").checked = false;
     });
 
-    // Отправляем данные формы на formcarry форму по событию submit тега form с is contactForm.
-    // Уходим назад по истории и предотвращаем событие submit, чтобы страница не перезагрузилась
+   
     contactForm.addEventListener("submit", function (event) {
         history.back();
         let formData = new FormData(contactForm);
@@ -62,8 +58,7 @@ window.addEventListener("DOMContentLoaded", function () {
          });
          event.preventDefault();
     });
-
-    // Проверяем текущее состояние истории. Если state = activeForm - открываем modalForm, если state = null - закрываем modalForm и убираем флажок с чекбокса
+  
     window.addEventListener("popstate", function () {
         if (history.state === "activeForm")
             $("#modalForm").modal("show");
